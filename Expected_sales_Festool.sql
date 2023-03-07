633cbcb12272bb135e3261e03aeb184f74e09ac9
jahidrazan <92856615+jahidrazan@users.noreply.github.com>
2023-03-07 16:57:39 +0100

    Add files via upload

---
+++ Expected_sales_Festool.sql
@@ -0,0 +1,130 @@
+-- this code can be used to estimate expected sales for 160 days
+
+-- add product id, product name, product price, sales class, average sales in stock, total sales and stock magento
+-- per product
+
+with t1 as (SELECT  DISTINCT(p.product_id)                               AS PRODUCT_ID, 
+
+                    CASE WHEN pl.label = 'new' THEN pl.label
+                         ELSE 'default' END                              AS NEW_PRODUCT_OR_NOT, 
+
+                    p.name_nl                                            AS PRODUCT_NAME,
+                    p.cost                                               AS PURCHASE_PRICE,
+                    s.name                                               AS DEFAULT_SUPPLIER,
+
+                    ltm.root_category                                    AS ROOT_CATEGORY, 
+                    pc1.label                                            AS SALES_CLASS_YEAR,  
+
+                    pc1.absolute                                         AS TOTAL_SALES_LAST_YEAR,
+                    ROUND(pc1.mean,2)                                    AS AVG_DAILY_SALES_LAST_YEAR,
+
+                    pc.absolute                                          AS TOTAL_SALES_IN_STOCK_LAST_YEAR,
+
+                    -- average sales in stock
+                    ROUND(pc.mean,2)                                     AS AVG_DAILY_SALES_WHILE_IN_STOCK,
+        
+                    p.stock_magento                                      AS STOCK_MAGENTO,
+
+                    -- the code is used for purchasing items for 160 days take it as the baseline to purchase
+                    -- for the next 160 days
+
+                    ROUND(pc.mean*160)                                   AS PRIMARY_QTY                              
+
+
+
+
+
+FROM `hbl-online.api.products` p
+
+LEFT JOIN `hbl-online.api.manufacturers` m
+ON p.manufacturer_id = m.manufacturer_id
+
+LEFT JOIN `hbl-online.gcp_sql_bi.ORDER_DATA_LTM` ltm 
+ON p.product_id = ltm.product_id
+
+LEFT JOIN `hbl-online.api.product_classifications` pc
+ON pc.product_id = p.product_id AND pc.period = 'year' AND pc.type = 'sales_in_stock'
+
+LEFT JOIN `hbl-online.api.product_classifications` pc1
+ON pc1.product_id = p.product_id AND pc1.period = 'year' AND pc1.type = 'sales'
+
+
+LEFT JOIN `hbl-online.api.suppliers` s
+ON p.default_supplier_id= s.supplier_id
+
+LEFT JOIN `hbl-online.api.product_labels` pl
+ON p.product_id = pl.product_id
+
+
+
+
+WHERE p.product_id NOT IN (SELECT DISTINCT(parent_product_id) FROM `hbl-online.api.product_set_items`) 
+      AND pc1.absolute > 0
+      AND m.name = 'Festool'
+
+),
+
+-- take the confirmed qty per product
+
+t2 as (SELECT DISTINCT ppo.product_id, 
+              CASE WHEN ppo.status_id = 4 THEN SUM(ppo.qty)  ELSE 0 END AS CONFIRMED
+
+        FROM `hbl-online.gcp_sql_bi.pending_purchase_orders` ppo 
+
+        WHERE ppo.product_id in (SELECT product_id
+                           FROM t1) AND 
+                           ppo.status_id = 4 AND ppo._sdc_deleted_at IS NULL
+        GROUP BY  status_id, product_id),
+
+-- take the backorder qty per product
+
+t3 as (SELECT DISTINCT product_id, 
+              CASE WHEN ppo.status_id = 2 THEN SUM(ppo.qty)  ELSE 0 END AS BACKORDER,
+             
+
+        FROM `hbl-online.gcp_sql_bi.pending_purchase_orders` ppo 
+        WHERE product_id in (SELECT product_id
+                           FROM t1) AND 
+                          ppo.status_id = 2 AND ppo._sdc_deleted_at IS NULL
+        GROUP BY  status_id, product_id),
+
+-- add the confirmed and backorder information to all individual
+-- products by joining table
+
+t4 as (SELECT  t1.*, 
+               CASE WHEN t2.CONFIRMED IS NULL THEN 0 ELSE t2.CONFIRMED END           AS CONFIRMED, 
+               CASE WHEN t3.BACKORDER IS NULL THEN 0 ELSE t3.BACKORDER END           AS BACKORDER, 
+               
+
+            FROM t1
+
+            LEFT JOIN t2
+            ON t1.product_id = t2.product_id
+
+            LEFT JOIN t3
+            ON t1.product_id = t3.product_id
+),
+
+
+
+t5 as (SELECT *,
+
+       -- when the stock magento >0 then subtruct CONFIRMED, BACKORDER and STOCK_MAGENTO from the baseline
+
+       CASE WHEN STOCK_MAGENTO >= 0 AND  (STOCK_MAGENTO + CONFIRMED+BACKORDER) < PRIMARY_QTY      THEN PRIMARY_QTY-(CONFIRMED+BACKORDER+STOCK_MAGENTO)
+
+          -- when the stock magento <0 then subtruct CONFIRMED, BACKORDER and add STOCK_MAGENTO to the baseline as it needs to be purchased
+            WHEN STOCK_MAGENTO < 0  AND  (CONFIRMED+BACKORDER) < (PRIMARY_QTY+ABS(STOCK_MAGENTO)) THEN PRIMARY_QTY+ABS(STOCK_MAGENTO)-(CONFIRMED+BACKORDER)
+            ELSE 0
+            END AS ORDER_QTY
+          FROM t4)
+      
+SELECT *,
+     ROUND(ORDER_QTY*PURCHASE_PRICE) AS ORDER_VALUE
+FROM t5
+
+-- exclude EOL products and products have order qty <0 that may arise for products only having rma  items
+WHERE DEFAULT_SUPPLIER != 'EOL / Discontinued' AND ORDER_QTY > 0
+ORDER BY ROUND(ORDER_QTY*PURCHASE_PRICE) DESC
+
+     
